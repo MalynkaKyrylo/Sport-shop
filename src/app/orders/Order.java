15e51e868be6c07d58f6833a6be2efa71963dd73
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-08-29 07:15:57 +0300

    Add Order class

---
+++ src/app/orders/Order.java
@@ -0,0 +1,30 @@
+package app.orders;
+
+
+import app.products.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Order {
+    List<Item> items = new ArrayList<>();
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public float getCost() {
+        float cost = 0.0f;
+
+        for (Item item : items) {
+            cost += item.price();
+        }
+        return cost;
+    }
+
+    public void showItems() {
+        for (Item item : items) {
+            System.out.print("Item: " + item.name());
+            System.out.print(", Producer: " + item.producer().brandName());
+            System.out.println(", Price: USD " + item.price());
+        }
+    }
+}
