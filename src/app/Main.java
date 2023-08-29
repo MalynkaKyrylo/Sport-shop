d7caa7fd5941696295ca0c63bcd86bf2db7c2a73
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-08-29 07:18:00 +0300

    Add Main class solution BuilderPattern

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,20 @@
 package app;
 
+import app.orders.Order;
+import app.orders.OrderBuilder;
+
 public class Main {
     public static void main(String[] args) {
-        
+        OrderBuilder orderBuilder = new OrderBuilder();
+
+        Order orderA = orderBuilder.prepareOrderA();
+        System.out.println("Order A");
+        orderA.showItems();
+        System.out.println("Total Cost: USD " + orderA.getCost());
+
+        Order orderB = orderBuilder.prepareOrderB();
+        System.out.println("\nOrder B");
+        orderB.showItems();
+        System.out.println("Total Cost: USD " + orderB.getCost());
     }
 }
