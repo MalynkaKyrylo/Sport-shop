fcfad89338fabe32a311305c7085d111e1753ea4
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-08-29 07:10:55 +0300

    Add classes Producers Brands

---
+++ src/app/producers/ActiveSports.java
@@ -0,0 +1,8 @@
+package app.producers;
+
+public class ActiveSports implements Producer {
+    @Override
+    public String brandName() {
+        return "Active Sports";
+    }
+}
---
+++ src/app/producers/SmartCaps.java
@@ -0,0 +1,8 @@
+package app.producers;
+
+public class SmartCaps implements Producer {
+    @Override
+    public String brandName() {
+        return "Smart Caps";
+    }
+}
